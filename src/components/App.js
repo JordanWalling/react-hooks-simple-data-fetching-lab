eff0552c2cf5679d6f31011c2e3080593300b90c
JordanWalling <jordanwalling1991@gmail.com>
2024-03-11 16:32:20 +1100

    complete data fetching with use Effect lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+function App() {
+  const [randomDog, setRandomDog] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchDog() {
+    setIsLoading(true);
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+    setRandomDog(data);
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    fetchDog();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={randomDog.message} alt="A Random Dog" />
+      )}
+    </>
+  );
+}
+
+export default App;
